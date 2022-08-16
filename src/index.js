b9111951281b90c29bf432bc3797f29cf4d09ee0
Jeremy Brown <jlbmagic@gmail.com>
2022-08-16 09:01:02 -0400

    added to-dos

--- src/index.js
+++ src/index.js
@@ -1,9 +1,27 @@
-//Here we're importing items we'll need. You can add other imports here.
+//TODO::get the survey questions from FileMaker as an array
 
-//The first function. Remove this.
-const btn = document.querySelector("button");
-btn.onclick = function () {
-  alert("You ran some JavaScript");
-};
+//TODO: create a slider for each question.
+  
+  //TODO: Get the 'slider' div into a variable.
+  //TODO: foEach: get the question, value, and id from the question.
+  //TODO: Create a <p> div where the question is displayed.
+  //TODO: Add some style to the <p> div.
+  //TODO: Put the question in the <p> div.
+  //TODO: put the value in a <span> div.
 
-//
+  
+  //TODO: create an input type="range" element.
+  //TODO: Assign type, min, max, className, and id to the input element.
+  //TODO: set the value of the input element to the value of the question.
+//TODO: use the inputs 'oninput' event to update the value of the <span> div and the FM field.
+//TODO: use the input's 'onchange' event to update the FM field.
+// TODO: Append the <p> div to the slider div.
+// TODO: Append the slider to the <div>
+
+
+window.loadSlider = function (json) {
+
+
+
+  
+}
