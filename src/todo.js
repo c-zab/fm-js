f86882bc4c0758f4ef66428de7566d9a714f7466
Jeremy Brown <jlbmagic@gmail.com>
2023-01-16 15:05:52 -0500

    added todo, updated parcel

---
+++ src/todo.js
@@ -0,0 +1,19 @@
+//TODO::get the survey questions from FileMaker as an array
+
+//TODO: create a slider for each question.
+  
+  //TODO: Get the 'slider' div into a variable.
+  //TODO: foEach: get the question, value, and id from the question.
+  //TODO: Create a <p> div where the question is displayed.
+  //TODO: Add some style to the <p> div.
+  //TODO: Put the question in the <p> div.
+  //TODO: put the value in a <span> div.
+
+  
+  //TODO: create an input type="range" element.
+  //TODO: Assign type, min, max, className, and id to the input element.
+  //TODO: set the value of the input element to the value of the question.
+//TODO: use the inputs 'oninput' event to update the value of the <span> div and the FM field.
+//TODO: use the input's 'onchange' event to update the FM field.
+// TODO: Append the <p> div to the slider div.
+// TODO: Append the slider to the <div>
